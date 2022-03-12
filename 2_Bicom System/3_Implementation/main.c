c6abb7df801811760404a073ef70a279bb809c8e
anusha2000 <47153476+anusha2000@users.noreply.github.com>
2022-03-12 07:54:05 +0530

    Update main.c

--- 2_Bicom System/3_Implementation/main.c
+++ 2_Bicom System/3_Implementation/main.c
@@ -1,24 +1,24 @@
 #include "function.h"
-static void ALLLED_ON(void);  // Switches ON all the LED's
-static void ALLLED_OFF(void); // Switches OFF all the LED's
-static void LEDCW_ON(void);	  // Switches ON LED'S in Clockwise Rotation
-static void LEDACW_ON(void);  // Switches ON LED's in AntiClockwise Rotation
+static void ALLLED_ON1(void);     
+static void ALLLED_OFF1(void);    
+static void LEDCW_ON1(void);	  
+static void LEDACW_ON1(void);     
 
-static void ALLLED_ON(void)   // Switches ON all the LED's
+static void ALLLED_ON1(void)     // Switches ON all the LED's
 {
 	led_on(LED_GREEN);
 	led_on(LED_ORANGE);
 	led_on(LED_RED);
 	led_on(LED_BLUE);
 }
-static void ALLLED_OFF(void)  // Switches OFF all the LED's
+static void ALLLED_OFF1(void)  // Switches OFF all the LED's
 {
 	led_off(LED_GREEN);
 	led_off(LED_ORANGE);
 	led_off(LED_RED);
 	led_off(LED_BLUE);
 }
-static void LEDCW_ON(void)   // Switches ON LED'S in Clockwise Rotation
+static void LEDCW_ON1(void)   // Switches ON LED'S in Clockwise Rotation
 {
 	led_on(LED_GREEN);
 	my_delay(350);
@@ -36,7 +36,7 @@ static void LEDCW_ON(void)   // Switches ON LED'S in Clockwise Rotation
 	my_delay(350);
 	led_off(LED_BLUE);
 }
-static void LEDACW_ON(void)   // Switches ON LED's in AntiClockwise Rotation
+static void LEDACW_ON1(void)   // Switches ON LED's in AntiClockwise Rotation
 {
 	led_on(LED_GREEN);
 	my_delay(350);
@@ -56,31 +56,31 @@ static void LEDACW_ON(void)   // Switches ON LED's in AntiClockwise Rotation
 }
 int main(void)
 {
-	led_init_all(); 
-	button_init();	
+	led_init_all();            // LED Initialisation
+	button_init();         	// Butoon Initialiasation
 	while (1)
 	{
-		int res;
-		res = button_count();				 
-		volatile int enc1 = encryption(res); 
-		if (enc1 == 6)
+		//int res;
+		volatile int res = button_count();				 // Button count collected
+		if (res == 1)                       
 		{
-			ALLLED_ON();
+			ALLLED_ON1();
 			res = 0;
 		}
-		else if (enc1 == 10)
+		else if (res == 2)
 		{
-			ALLLED_OFF();
+			ALLLED_OFF1();
 			res = 0;
 		}
-		else if (enc1 == 25)
+		else if (res == 3)
 		{
-			LEDCW_ON();
+			LEDCW_ON1();
 			res = 0;
 		}
-		else if (enc1 == 34)
+		else if (res == 4)
 		{
-			LEDACW_ON();
+			LEDACW_ON1();
 			res = 0;
 		}
 	}
+}
